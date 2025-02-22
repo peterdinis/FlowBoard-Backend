84cf8f13dc17d68052f53ce62c55637d486c2537
peterdinis <petter1@pobox.sk>
2025-02-22 10:49:20 +0100

    feat: Create helper user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+}
