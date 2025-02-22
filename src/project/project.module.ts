990ac7a50c611dbe49a18231aad59862feab2d6f
peterdinis <petter1@pobox.sk>
2025-02-22 20:29:15 +0100

    feat: Updating project folder

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { ProjectService } from "./project.service";
+import { ProjectController } from "./project.controller";
 
 @Module({
-    imports: [PrismaModule]
+    imports: [PrismaModule],
+    providers: [ProjectService],
+    controllers: [ProjectController]
 })
 
 export class ProjectModule {}
